8583c8cf5036dea98287adfd1b8cc85cffd50157
Dafovi <swan364@hotmail.com>
2023-02-05 01:01:27 +0100

    Sprites & URP

--- Assets/Scripts/FinalScene/TriggerOption.cs
+++ Assets/Scripts/FinalScene/TriggerOption.cs
@@ -6,7 +6,7 @@ using UnityEngine.Events;
 public class TriggerOption : MonoBehaviour
 {
     [SerializeField] private UnityEvent OnEnter;
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.CompareTag("Player"))
         {
