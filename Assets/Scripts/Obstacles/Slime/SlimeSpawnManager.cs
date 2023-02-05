83a97fbde1a7cee86cf8331ddaa04e8344faefa3
Tovarishch Alcazarovich <62358700+TyElessar@users.noreply.github.com>
2023-02-05 04:23:56 +0100

    estenoo

--- Assets/Scripts/Obstacles/Slime/SlimeSpawnManager.cs
+++ Assets/Scripts/Obstacles/Slime/SlimeSpawnManager.cs
@@ -3,45 +3,35 @@ using UnityEngine;
 public class SlimeSpawnManager : MonoBehaviour {
 
   [SerializeField] private GameObject sisterFungiRef_;
-  [SerializeField] private GenericPool pool_;
+  [SerializeField] private GenericPool slimePoolRef_;
 
   public GameObject leftSpawn_;
   public GameObject rightSpawn_;
 
   private Vector3 spawnPosition_;
-  private int difficultToSpawn_;
   public int spawnDirection_;
+  public int spawnedSlimes_;
 
   void Start() {
-    pool_ = GetComponent<GenericPool>();
-
-  }
-
-  void Update(){
-    if(GameManager.Instance.currentDifficulty_ > difficultToSpawn_){
-
-    }
-
-    if(Input.GetButtonDown("Jump")){
-      SetSlimeSpawn();
-    }
-
+    slimePoolRef_ = GetComponent<GenericPool>();
+    GameManager.Instance.EnemyHit += SetSlimeSpawn;
   }
 
   void SetSlimeSpawn(){
-
-    switch(Random.Range(0, 2)){
-      case 0: 
-        spawnPosition_ = leftSpawn_.transform.position;
-        spawnDirection_ = 1;
-        break;
-      case 1:
-        spawnPosition_ = rightSpawn_.transform.position;
-        spawnDirection_ = -1;
-        break;
+    if(spawnedSlimes_ < slimePoolRef_.pool_.Count){
+      switch(Random.Range(0, 2)){
+        case 0: 
+          spawnPosition_ = leftSpawn_.transform.position;
+          spawnDirection_ = 1;
+          break;
+        case 1:
+          spawnPosition_ = rightSpawn_.transform.position;
+          spawnDirection_ = -1;
+          break;
+      }
+      spawnedSlimes_++;
+      slimePoolRef_.GetFromPool(spawnPosition_);
     }
-
-    pool_.GetFromPool(spawnPosition_);
   }
 
 
